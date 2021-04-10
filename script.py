9551614a7679983fb2a5ed124df9e45b15be172f
GhitaB <ghitabizau27@gmail.com>
2021-04-10 21:34:54 +0300

    Solution for list of domains.

--- script.py
+++ script.py
@@ -1,7 +1,18 @@
 import whois
 
-domain = whois.query('google.com')
-print(domain.__dict__)
+with open("list-of-domains.txt") as file_in:
+    for item in file_in:
+        line = item.rstrip()
+        domain_name = line
+        try:
+            domain_info = whois.query(domain_name)
+            domain_expiration = domain_info.expiration_date
+        except whois.exceptions.UnknownTld:
+            domain_expiration = "error: Unknown TLD"
+
+        print("{} - {}".format(domain_name, domain_expiration))
+
+# print(domain.__dict__)
 # {
 #         'expiration_date': datetime.datetime(2020, 9, 14, 0, 0),
 #         'last_updated': datetime.datetime(2011, 7, 20, 0, 0),
